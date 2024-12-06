e82ab311f600eeb229fa2021398e138f16ba3503
Adithya Bhat <adithyabhat7@gmail.com>
2024-12-06 14:47:32 -0800

    include datetime

--- pages/Top Nutritionists.py
+++ pages/Top Nutritionists.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine, text
+from datetime import datetime, timedelta
 import tempfile
+
 
 # Load environment variables from .env file (if using)
 
